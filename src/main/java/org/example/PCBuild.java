7653252693b2476864bad369e8328a94894e5197
ddos5 <yoiu2577@gmail.com>
2026-02-21 17:01:57 +0100

    Added class PCBuild with method addComponent and arraylist

---
+++ src/main/java/org/example/PCBuild.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PCBuild implements ComponentAdder{
+    private List<Component> components = new ArrayList<>();
+    public double calculateTotalPower(){
+        double sum = 0;
+        for(Component x : components){
+            sum += x.getBasePowerConsumption();
+        }
+        return sum;
+    }
+    @Override
+    public void addComponent(Component c){
+        components.add(c);
+    }
+}
