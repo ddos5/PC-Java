2b615fdcff21a76935998f6d210bfb97d18ef325
ddos5 <yoiu2577@gmail.com>
2026-02-21 18:41:27 +0100

    Added class PowerSupply to check available maxWottage in PC

--- src/main/java/org/example/PowerSupply.java
+++ src/main/java/org/example/PowerSupply.java
@@ -1,4 +1,14 @@
 package org.example;
 
-public class PowerSupply {
+public class PowerSupply extends Component{
+    private int maxWattage;
+    public PowerSupply(String brand, String model, int price){
+        super(brand, model, price);
+    }
+    public int getMaxWattage(){
+        return maxWattage;
+    }
+    public void setMaxWattage(int maxWattage){
+        this.maxWattage = maxWattage;
+    }
 }
