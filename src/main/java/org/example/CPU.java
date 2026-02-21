5b4c6fba30e6a88ef51e54a3c4588e173ce63e22
ddos5 <yoiu2577@gmail.com>
2026-02-21 18:37:31 +0100

    Added some changes

--- src/main/java/org/example/CPU.java
+++ src/main/java/org/example/CPU.java
@@ -3,13 +3,17 @@ package org.example;
 public class CPU extends Component implements Overclockable{
     private int cores;
     private double frequency;
-    private int socket;
-    public CPU(String brand, String model, int price, double basePowerConsumption, int cores, int frequency, int socket){
+    private String socket;
+    public CPU(String brand, String model, int price, double basePowerConsumption, int cores, double frequency, String socket){
         super(brand, model, price, basePowerConsumption);
         this.cores = cores;
         this.frequency = frequency;
         this.socket = socket;
     }
+    public CPU(){
+
+    }
+
     @Override
     public void boostPerformance(double factor){
         if(factor > 1.5) {
@@ -22,6 +26,22 @@ public class CPU extends Component implements Overclockable{
             if (this.basePowerConsumption > 350)
                 System.out.println("WARNING: Thermal throttling! CPU might be damaged!");
         }
+    }
+
+    /*getters setters*/
+
+    public int getCores(){
+        return cores;
+    }
+    public double getFrequency(){
+        return frequency;
+    }
+    public String getSocket(){
+        return socket;
+    }
+
+    public String getInfo(){
+        return "Brand: " + getBrand() + " | Model: " + getModel() + " | Price: " + getPrice() + " | Power cons.: " + String.format("%.2f", basePowerConsumption) + " | Cores: " + cores + " | Frequency: " + String.format("%.2f", frequency) + " | String: " + socket;
     }
 
 }
