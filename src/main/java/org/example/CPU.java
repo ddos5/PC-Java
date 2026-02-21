4403af908a3aa10ac46ccd5c99b7c8ee204c6029
ddos5 <yoiu2577@gmail.com>
2026-02-21 16:16:04 +0100

    Added implementation of interface Overclockable

--- src/main/java/org/example/CPU.java
+++ src/main/java/org/example/CPU.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class CPU extends Component implements Overclockable{
     private int cores;
-    private int frequency;
+    private double frequency;
     private int socket;
     public CPU(String brand, String model, int price, double basePowerConsumption, int cores, int frequency, int socket){
         super(brand, model, price, basePowerConsumption);
@@ -12,9 +12,16 @@ public class CPU extends Component implements Overclockable{
     }
     @Override
     public void boostPerformance(double factor){
-        this.basePowerConsumption *= (factor * 1.2);
-        if(this.basePowerConsumption > 350)
-            System.out.println("WARNING: Thermal throttling! CPU might be damaged!");
+        if(factor > 1.5) {
+            this.frequency = 0.5;
+            this.basePowerConsumption /= factor;
+        }
+        else {
+            this.frequency *= factor;
+            this.basePowerConsumption *= (factor * 1.2);
+            if (this.basePowerConsumption > 350)
+                System.out.println("WARNING: Thermal throttling! CPU might be damaged!");
+        }
     }
 
 }
