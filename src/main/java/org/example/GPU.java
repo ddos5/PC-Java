bdb89ee2f9c03e8798e4d4eb265f7f803a3e116d
ddos5 <yoiu2577@gmail.com>
2026-02-21 18:39:27 +0100

    GPU changes

--- src/main/java/org/example/GPU.java
+++ src/main/java/org/example/GPU.java
@@ -3,15 +3,27 @@ package org.example;
 public class GPU extends Component implements Overclockable{
     private int vramSize;
     private boolean hasRayTracing;
-    private int clockSpeed;
-    public GPU(String brand, String model, int price, double basePowerConsumption, int vramSize, boolean hasRayTracing, int clockSpeed){
+    private double clockSpeed;
+    public GPU(String brand, String model, int price, double basePowerConsumption, int vramSize, boolean hasRayTracing, double clockSpeed){
         super(brand, model, price, basePowerConsumption);
         this.vramSize = vramSize;
         this.hasRayTracing = hasRayTracing;
         this.clockSpeed = clockSpeed;
     }
+    public GPU(){
+
+    }
     @Override
     public void boostPerformance(double factor){
-        
+        if(factor > 1.5){
+            this.clockSpeed /= factor;
+            this.basePowerConsumption = 15;
+        }
+        else{
+            this.clockSpeed *= factor;
+            this.basePowerConsumption *= factor*factor;
+            if(this.basePowerConsumption > 500)
+                System.out.println("WARNING: Thermal throttling! GPU might be damaged!");
+        }
     }
 }
