ae1946c6a36b7dbdf8845328285e36982e42f315
ddos5 <yoiu2577@gmail.com>
2026-02-23 02:02:57 +0100

    Added method installCpu to compare CPUs, also this method is used in PCBuild class to check compatibility

--- src/main/java/org/example/Motherboard.java
+++ src/main/java/org/example/Motherboard.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Motherboard extends Component{
     private String socket;
+    private PCBuild newCpu;
     public Motherboard(String brand, String model, int price, double basePowerConsumption, String socket){
         super(brand, model, price, basePowerConsumption);
         this.socket = socket;
@@ -10,7 +11,14 @@ public class Motherboard extends Component{
 
     }
 
-    public boolean compareSocket(CPU cpu){
-        return cpu.getSocket().equals(socket);
+    public String getSocket(){
+        return socket;
+    }
+
+    public void installCpu(CPU cpu) throws IncompatibleSocketException{
+        if(!cpu.getSocket().equals(this.socket)){
+            throw new IncompatibleSocketException("Your socket is different, needed socket: " + getSocket());
+        }
+        System.out.println("CPU installed!");
     }
 }
